fa0028b1935b13a13ed2ee5ea6a15f30f5e855e2
Israel <isr.gr1999@outlook.com>
2024-03-29 18:29:07 -0600

    updated modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import CardProyect from "./CardProyect";
 const Modal = ({ children, modal, title }) => {
   return (
-    <div className="modal fade" id={modal} tabIndex="-1" aria-hidden="true">
-      <div className="modal-content">
-        <div className="modal-header">
-          <label className="h5">{title}</label>
-          <button
-            className="btn btn-close"
-            type="button"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+    <div className="modal fade bd-example-modal-lg" id={modal} tabIndex="-1" aria-hidden="true">
+      <div className="modal-dialog modal-lg">
+        <div className="modal-content">
+          <div className="modal-header">
+            <label className="h5">{title}</label>
+            <button
+              className="btn btn-close"
+              type="button"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     </div>
   );
